7b565ab30f35c667a79bafef7b2c1cbc3e01ab1a
NAPTRIX <31538258+NAPTRIX@users.noreply.github.com>
2023-02-27 12:54:22 +0100

    Update main.js
    it now processes each page individually to make sure they're being concatenated properly

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const fileInput = document.getElementById('fileInput');
 const playButton = document.getElementById('playButton');
 const outputArea = document.getElementById('outputArea');
-let i = 0;
-let speed = 50;
-let pitch = 1;
 
 function typeWriter(element, text, speed) {
   let i = 0;
@@ -16,6 +13,20 @@ function typeWriter(element, text, speed) {
   }
   write();
 }
+/* This is experimental 
+function typeWriter(element, text, speed) {
+  let i = 0;
+  const delay = speed / text.length;
+  function write() {
+    element.textContent += text.charAt(i);
+    i++;
+    if (i < text.length) {
+      setTimeout(write, delay);
+    }
+  }
+  write();
+}*/
+
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js';
 
@@ -33,47 +44,49 @@ speechSynthesis.onvoiceschanged = function() {
   pitchSlider.value = pitch;
 };
 
-playButton.addEventListener('click', () => {
-
+playButton.addEventListener("click", () => {
   const file = fileInput.files[0];
   const reader = new FileReader();
-  reader.addEventListener('load', () => {
-    pdfjsLib.getDocument(reader.result).promise.then(function(pdf) {
+  reader.addEventListener("load", () => {
+    pdfjsLib.getDocument(reader.result).promise.then(function (pdf) {
       const maxPages = pdf.numPages;
       let currentPage = 1;
-      let pageText = '';
 
-      function playPage(page) {
-        page.getTextContent().then(function(textContent) {
-          let item;
-
-          for (item of textContent.items) {
-            pageText += item.str;
-          }
-
-          const utterance = new SpeechSynthesisUtterance(pageText);
-          utterance.voice = speechSynthesis.getVoices()[voicePicker.value];
-          utterance.pitch = pitchSlider.value;
-          window.speechSynthesis.speak(utterance);
-
-          //outputArea.textContent = pageText;
-          console.log(pageText.length);
-         if (pageText.length){
-           typeWriter(outputArea,pageText,350)
-          }
-        }).catch(console.error);
+      // recrusive function 
+      function processPage(pageNum) {
+        if (pageNum > maxPages) {
+          return; // All pages have been processed
+        }
+        pdf.getPage(pageNum).then(function (page) {
+          page.getTextContent().then(function (textContent) {
+            let pageText = "";
+            for (const item of textContent.items) {
+              pageText += item.str;
+            }
+            const utterance = new SpeechSynthesisUtterance(pageText);
+            utterance.voice = speechSynthesis.getVoices()[voicePicker.value];
+            utterance.pitch = pitchSlider.value;
+            window.speechSynthesis.speak(utterance);
+            typeWriter(outputArea, pageText, 1);
+            // typewriter effect after all text has been concatenated 
+            utterance.onend = function () {
+              
+              // process the next page
+              processPage(pageNum + 1);
+            };
+          }).catch(console.error);
+        });
       }
 
-      pdf.getPage(currentPage).then(playPage);
+      processPage(1); // start processing the first page
     });
   });
-
   reader.readAsArrayBuffer(file);
 });
+;
 
 // Update the pitch when the user moves the pitch slider
 const pitchSlider = document.getElementById('pitchSlider');
 pitchSlider.addEventListener('input', () => {
   pitch = pitchSlider.value;
 });
-
